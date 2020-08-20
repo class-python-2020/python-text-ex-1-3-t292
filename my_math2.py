935e61b69e4e5900425a9e809ce33867b5604117
takita ryousuke <r.takita.sys20@morijyobi.ac.jp>
2020-08-20 15:25:11 +0900

    おわり

---
+++ my_math2.py
@@ -0,0 +1,11 @@
+def my_oiw(x,y)):
+    return x**y
+if _
+ame_\=="__main__":
+    x,y,exp=2,5,32
+    ans=my_pow(x,y)
+    print("test my_pow({},{})->{}exp:----".format(x,y,ans,exp),end="")
+    if ans == exp:
+        print("text ok")
+        else:
+            print("test fail"
